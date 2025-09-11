913b5fe78b8ac9e7702620dd0a5dd76084d63cc8
JuanCasZa <juanjose-castro2@hotmail.com>
2025-09-10 20:09:44 -0500

    Cambios

--- obj/Debug/net9.0/PruebaConexion.AssemblyInfo.cs
+++ obj/Debug/net9.0/PruebaConexion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaConexion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500d9dc705419d3dd6d63ebb626a5065f187a521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eceb95441f2ee0e7193992813359f3ea6545c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaConexion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaConexion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
